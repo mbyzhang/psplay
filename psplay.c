812b750024674e224d38d98d0ab9e98ecf43d76f
Peter Zhang <mbyzhang@outlook.com>
2021-11-13 15:02:50 +0000

    set thread priority to max

--- psplay.c
+++ psplay.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <sched.h>
+
 #include "cpu_spinner.h"
 #include "fsk.h"
 
 int main(int argc, char* argv[]) {
     cpu_spinner_t spinner;
     fsk_t fsk;
+
+    const int policy = SCHED_RR;
+    struct sched_param param = {
+        .sched_priority = sched_get_priority_max(policy)
+    };
+    pthread_setschedparam(pthread_self(), policy, &param);
+
     cpu_spinner_init(&spinner, 0);
     fsk_init(&fsk, &spinner);
 
