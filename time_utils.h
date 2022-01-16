6dd906ef9edf5398be88bfe3ee75f4c6d92d3e4b
Peter Zhang <mbyzhang@outlook.com>
2022-01-16 00:28:14 +0000

    time_utils: make all functions static inline

--- time_utils.h
+++ time_utils.h
@@ -7,7 +7,7 @@
 
 #define GIGA 1000000000L
 
-void timespec_diff(struct timespec* a, struct timespec b) {
+static inline void timespec_diff(struct timespec* a, struct timespec b) {
     a->tv_sec = b.tv_sec - a->tv_sec;
     a->tv_nsec = b.tv_nsec - a->tv_nsec;
     if (a->tv_sec < 0L) {
@@ -24,22 +24,22 @@ void timespec_diff(struct timespec* a, struct timespec b) {
     } else {}
 }
 
-void timespec_step(struct timespec* a, struct timespec b) {
+static inline void timespec_step(struct timespec* a, struct timespec b) {
     a->tv_sec += b.tv_sec;
     a->tv_nsec += b.tv_nsec;
     a->tv_sec += a->tv_nsec / GIGA;
     a->tv_nsec %= GIGA;
 }
 
-struct timeval us_to_timeval(uint64_t us) {
+static inline struct timeval us_to_timeval(uint64_t us) {
     return (struct timeval){ us / 1000000L, us % 1000000UL };
 }
 
-struct timeval timespec_to_timeval(struct timespec a) {
+static inline struct timeval timespec_to_timeval(struct timespec a) {
     return (struct timeval){ a.tv_sec, a.tv_nsec / 1000L };
 }
 
-struct timespec timeval_to_timespec(struct timeval a) {
+static inline struct timespec timeval_to_timespec(struct timeval a) {
     return (struct timespec){ a.tv_sec, a.tv_usec * 1000L };
 }
 
