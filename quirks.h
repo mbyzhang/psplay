004385d3f18d528422d7edcb3555e44de5b4ffee
Peter <me@mby.pw>
2022-04-24 21:35:19 +0100

    quirks: fix error handling

--- quirks.h
+++ quirks.h
@@ -5,10 +5,10 @@
 #ifdef __linux__
 #include <sys/sysinfo.h>
 
-static int file_read(const char* filename, char* buf, size_t cnt) {
+static int file_read(const char* filename, char* buf, size_t maxlen) {
     FILE* fp = fopen(filename, "r");
-    if (fp == NULL) return 1;
-    char* ret = fgets(buf, cnt, fp);
+    if (fp == NULL) return -1;
+    char* ret = fgets(buf, maxlen, fp);
     if (ret != NULL) buf[strcspn(buf, "\n")] = 0;
     fclose(fp);
     return 1;
@@ -16,8 +16,9 @@ static int file_read(const char* filename, char* buf, size_t cnt) {
 
 static int file_write(const char* filename, const char* buf) {
     FILE* fp = fopen(filename, "w");
-    if (fp == NULL) return 0;
-    int ret = fputs(buf, fp);
+    if (fp == NULL) return -1;
+    setvbuf(fp, NULL, _IONBF, 0);
+    int ret = fprintf(fp, "%s\n", buf);
     fclose(fp);
     return ret;
 }
@@ -32,17 +33,18 @@ static void linux_cpufreq_governor_set_to_performance() {
         snprintf(filename, sizeof(filename), "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor", i);
 
         ret = file_read(filename, governor_current, sizeof(governor_current));
-        if (ret == 0) {
+        if (ret <= 0) {
             perror("Failed to get current CPUFreq scaling governor");
             continue;
         }
         if (strncmp(governor_current, governor_target, sizeof(governor_current)) != 0) {
             printf("Setting CPUFreq scaling governor for CPU %d from %s to %s\n", i, governor_current, governor_target);
             ret = file_write(filename, governor_target);
-            if (ret == 0) {
+            if (ret <= 0) {
                 perror("Failed to set CPUFreq scaling governor, performance may be affected");
                 continue;
             }
+            printf("%d\n", ret);
         }
     }
 }
